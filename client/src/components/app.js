f26321fdf6447cba901822c6e572966b132a307f
Tiffany Kim <minikim9@hotmail.com>
2018-12-20 15:30:41 -0800

    Setup npm scripts

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/ff.jpeg';
 
 const App = () => (
     <div>
         <div className="app">
             <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+            <h1>Welcome to Gold Fish Land</h1>
         </div>
     </div>
 );
