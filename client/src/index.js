aa286172e954c158129c093216b12da8e6d69f47
Tiffany Kim <minikim9@hotmail.com>
2018-12-20 16:56:36 -0800

    Completed initial server and client setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/app';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers';
+import think from './middleware/think';
+
+const store = createStore(rootReducer, {}, applyMiddleware(think));
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}><Router><App /></Router></Provider>,
     document.getElementById('root')
 );
